00a478a8746a5f7faba87016a0010def82e7cba5
eks05 <eungyo307@naver.com>
2021-08-23 08:51:59 +0900

    3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
+      <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=3a29c2757bfffc6de32272f818e1bf2a&libraries=services,clusterer,drawing"></script>
     </div>
   );
 }
